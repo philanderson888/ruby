df5766fe7ea0572ba1f7e7e54cd4e6b8beddd5ce
philandersonsparta <panderson@spartaglobal.com>
2019-01-15 16:56:40 +0000

    update sinatra to include routes

---
+++ ruby_81_sinatra_02.rb
@@ -0,0 +1,43 @@
+require "sinatra"
+require "sinatra/contrib"
+require "sinatra/reloader" if development?
+
+# creating the index page
+
+# if get request to root of website return 'Hello World'
+# show all
+get "/" do
+	"<h1>Hello World 4</h1>"
+end
+
+#new
+get "/new" do
+	"this is the new page"
+end
+
+# show individual
+get "/:id" do
+	id = params[:id]
+	"show me page for : #{id}"
+end
+
+# update
+get "/:id/edit" do 
+	id=params[:id]
+	"edit page for #{id}"
+end
+
+#create
+post "/" do
+	"create"
+end
+
+# update
+put "/:id" do
+	"UPDATE"
+end
+
+#delete
+delete "/:id" do 
+	"DELETE"
+end
